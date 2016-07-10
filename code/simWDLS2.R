e9f68a68a9ce46b20e2cbddef86ea82b2047f38b
studyvin <jstudyvin@west-inc.com>
2016-07-10 15:18:20 -0600

    start sim on S2

---
+++ code/simWDLS2.R
@@ -0,0 +1,149 @@
+##########################################################
+## Jared Studyvin
+## 9 June 2016
+## Test the weighted distribution
+##########################################################
+rm(list=ls())
+
+library(plyr)
+
+##userPath <- '~/GoogleDrive/wind/fatality/areaCorrection/likelihoodAC/'
+userPath <- 'D:/studyvin/likelihoodAC/'
+dataPath <- paste0(userPath,'data/')
+outPath <- paste0(userPath,'output/')
+codePath <- paste0(userPath,'code/')
+
+
+
+source(paste0(codePath,'weightFun.R'))
+source(paste0(codePath,'weightedDistribution.R'))
+source(paste0(codePath,'getStartValue.R'))
+source(paste0(codePath,'summaryFun.R'))
+
+
+
+##listEl <- llogListSmall[[1]]
+##dat <- subset(listEl,plotType=='RP'&piHat==.7)
+
+estWD <- function(listEl,...){
+
+    outList <- dlply(listEl,~plotType+piHat,function(subDat,...){
+                       ##allDist <- c('rayleigh','gamma','weibull','llog','norm','gompertz')
+                       allDist <- c('gamma','weibull','llog','norm')
+                       plotType <- as.character(subDat$plotType[1])
+                       piHat <- subDat$piHat[1]
+                       message('The plot type is ',plotType,' and piHat is ',piHat)
+                       fatDist <- subDat$distance
+                       out <- ldply(allDist,weightedDistribution,fatDist=fatDist,...,type=plotType)
+                       out$n <- length(fatDist)
+                       out$plotType <- plotType
+                       out$piHat <- piHat
+                       return(out)
+                   },...)
+
+    return(outList)
+}
+
+
+
+
+
+
+library(parallel)
+library('doSNOW')
+detectCores(all.tests = TRUE, logical = TRUE)
+
+
+
+#############################################################################
+## norm
+#############################################################################
+## small
+
+load(paste0(dataPath,'normSmall.Rdata'))
+ls()
+
+cl <- makeCluster(5)
+registerDoSNOW(cl)
+Sys.time()
+system.time(
+    normSmallResult <- llply(normListSmall,estWD,weightFun=weightFun,subdivisions=10000,.paropts=list(.export=c('getStartValue','weightedDistribution')),.parallel=TRUE)
+)
+stopCluster(cl)
+
+save(normSmallResult,normParam,file=paste0(outPath,'normSmallWD.Rdata'))
+
+
+## mid
+load(paste0(dataPath,'normMid.Rdata'))
+
+
+cl <- makeCluster(5)
+registerDoSNOW(cl)
+Sys.time()
+system.time(
+    normMidResult <- llply(normListMid,estWD,weightFun=weightFun,subdivisions=10000,.paropts=list(.export=c('getStartValue','weightedDistribution')),.parallel=TRUE)
+)
+stopCluster(cl)
+
+save(normMidResult,normParam,file=paste0(outPath,'normMidWD.Rdata'))
+
+## big
+load(paste0(dataPath,'normBig.Rdata'))
+
+cl <- makeCluster(5)
+registerDoSNOW(cl)
+Sys.time()
+system.time(
+    normBigResult <- llply(normListBig,estWD,weightFun=weightFun,subdivisions=10000,.paropts=list(.export=c('getStartValue','weightedDistribution')),.parallel=TRUE)
+)
+stopCluster(cl)
+
+save(normBigResult,normParam,file=paste0(outPath,'normBigWD.Rdata'))
+
+
+#############################################################################
+## gamma
+#############################################################################
+## small
+
+load(paste0(dataPath,'gammaSmall.Rdata'))
+ls()
+
+cl <- makeCluster(10)
+registerDoSNOW(cl)
+Sys.time()
+system.time(
+    gammaSmallResult <- llply(gammaListSmall,estWD,weightFun=weightFun,subdivisions=10000,.paropts=list(.export=c('getStartValue','weightedDistribution')),.parallel=TRUE)
+)
+stopCluster(cl)
+
+save(gammaSmallResult,gammaParam,file=paste0(outPath,'gammaSmallWD.Rdata'))
+
+
+## mid
+load(paste0(dataPath,'gammaMid.Rdata'))
+
+
+cl <- makeCluster(10)
+registerDoSNOW(cl)
+Sys.time()
+system.time(
+    gammaMidResult <- llply(gammaListMid,estWD,weightFun=weightFun,subdivisions=10000,.paropts=list(.export=c('getStartValue','weightedDistribution')),.parallel=TRUE)
+)
+stopCluster(cl)
+
+save(gammaMidResult,gammaParam,file=paste0(outPath,'gammaMidWD.Rdata'))
+
+## big
+load(paste0(dataPath,'gammaBig.Rdata'))
+
+cl <- makeCluster(5)
+registerDoSNOW(cl)
+Sys.time()
+system.time(
+    gammaBigResult <- llply(gammaListBig,estWD,weightFun=weightFun,subdivisions=10000,.paropts=list(.export=c('getStartValue','weightedDistribution')),.parallel=TRUE)
+)
+stopCluster(cl)
+
+save(gammaBigResult,gammaParam,file=paste0(outPath,'gammaBigWD.Rdata'))
