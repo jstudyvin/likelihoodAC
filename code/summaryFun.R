cdb637f3885fbb985006307d6a017f71cc4df6cd
studyvin <jstudyvin@west-inc.com>
2016-07-22 13:07:02 -0600

    note to me

--- code/summaryFun.R
+++ code/summaryFun.R
@@ -95,7 +95,7 @@ CDF <- function(q,parm,distn){
                   gamma=pgamma(q=q,shape=parm[1],rate=parm[2]),
                   weibull=pweibull(q=q,shape=parm[1],scale=parm[2]),
                   llog=pllog(q=q,shape=parm[1],scale=parm[2]),
-                  norm=pnorm(q=q,mean=parm[1],sd=parm[2]),
+                  norm=pnorm(q=q,mean=parm[1],sd=parm[2]), ## this might need to be truncated.
                   gompertz=pgompertz(q=q,scale=parm[1],shape=parm[2])
                   )# end switch
     return(cdf)
